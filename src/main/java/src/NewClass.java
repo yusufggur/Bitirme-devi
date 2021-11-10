5e88cc38de09b3d3ff8dbb06e8f5b22c3b3e33e8
yusufggur <2018123031@cumhuriyet.edu.tr>
2021-11-10 16:41:01 +0300

    değişiklik yapıldı

--- src/main/java/src/NewClass.java
+++ src/main/java/src/NewClass.java
@@ -3,7 +3,11 @@ package src;
 
 public class NewClass {
     public static void main(String[] args){
+        int c =10;
+        int b = 20;
+        System.out.println("sonuç " + c+b);
         String a = "yusuf gür";
         System.out.println(a);
     }
+    
 }
